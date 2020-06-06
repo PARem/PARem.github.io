53bf742cda740e12b1e51f1da8c805a9f3322eb6
Petr <p_remez2@ukr.net>
2020-06-06 14:12:29 +0300

    v2

--- works/snake/js/fruit.js
+++
@@ -1,6 +0,0 @@
-class Fruit extends Elem{
-    constructor(matrix, cords){
-        super(matrix, cords);
-        this.value = 'fruit';
-    }
-}
--- works/snake/js/wall.js
+++
@@ -1,6 +0,0 @@
-class Wall extends Elem{
-    constructor(matrix, cords){
-        super(matrix, cords);
-        this.value = 'wall';
-    }
-}
